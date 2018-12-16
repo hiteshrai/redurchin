f4bcf20f2d764e0dbc0df9888277b0e08ba01b4f
Hitesh Rai <hiteshrai1013@gmail.com>
2018-12-15 23:50:07 -0700

    Added software downsampling. -Changed MCLK frequency to 100KHz, so that SPI transfer completes before ready pin goes high. Before MCLK was running on 1.6MHz and we were getting Ready pin interrupt every 20uS and not letting SPI transfer to complete and hence giving bad data.

--- app/main.c
+++ app/main.c
@@ -9,12 +9,15 @@
 /*******************************************************************************
 * Definitions
 ******************************************************************************/
-#define MCLK_FREQ              1600000U
+#define MCLK_FREQ              100000U
+#define HARDWARE_DOWNSAMPLING  32
 
 /*******************************************************************************
 * Variables
 ******************************************************************************/
 static volatile bool analog_ready_data = false;
+static uint32_t sample_frequency = 1;
+static uint32_t transfer_sample_count;
 
 /*******************************************************************************
 * Prototypes
@@ -23,6 +26,17 @@ static volatile bool analog_ready_data = false;
 /*******************************************************************************
 * Code
 ******************************************************************************/
+static uint32_t get_sample_count_from_frequency(uint32_t frequency)
+{
+	uint32_t max_frequency = MCLK_FREQ / HARDWARE_DOWNSAMPLING;
+	if (frequency > max_frequency)
+	{
+		frequency = max_frequency;
+	}
+	uint32_t samples = MCLK_FREQ / HARDWARE_DOWNSAMPLING / frequency;
+	return samples;
+}
+
 static void analog_ready_callback(void)
 {
 	// Set the flag indicating that an analog read is available
@@ -31,37 +45,58 @@ static void analog_ready_callback(void)
 
 int main(void)
 {
+	uint64_t reading_nV_sum = 0;
+	bool missed_reading = false;
+	uint32_t sample_count = 0;
+
 	hardware_init();
 	usb_cdc_init();
 	clock_init();
 	
-	uint32_t last_usb_send_tick = clock_get_tick();
-	analog_init();	
-	
+	analog_init();		
 	analog_start(MCLK_FREQ, analog_ready_callback);
-	uint32_t last_time = clock_get_tick();
+	
+	transfer_sample_count = get_sample_count_from_frequency(sample_frequency);
 	
 	while (1)
 	{
 		usb_cdc_task();
 		uint32_t current_time = clock_get_tick();
+		int len;
 		
 		if (analog_ready_data)
 		{
+			char buffer[50];
 			uint64_t reading_nV;
+			sample_count++;
 			if (analog_get_reading(&reading_nV))
 			{
-				if (clock_get_elapsed_time_ms(current_time, last_time) > 1000)
+				reading_nV_sum += reading_nV;
+			}
+			else
+			{
+				missed_reading = true;
+			}
+			if (sample_count >= transfer_sample_count)
+			{
+				if (missed_reading)
 				{
-					last_time = current_time;
-					
+					len = snprintf(buffer, sizeof(buffer) - 1, "Missed a reading\r\n");
+				}
+				else
+				{
+					uint64_t reading_nV_avg = reading_nV_sum / transfer_sample_count;
 					uint32_t integer_voltage = reading_nV / 1000000000;
 					uint32_t fraction_voltage = reading_nV % 1000000000;
-					char buffer[50];
-					int len = snprintf(buffer, sizeof(buffer) - 1, "Voltage: %ld.%09ld\r\n", integer_voltage, fraction_voltage);
-					usb_cdc_write((uint8_t *)buffer, len);	
-				}				
+					len = snprintf(buffer, sizeof(buffer) - 1, "Voltage: %ld.%09ld\r\n", integer_voltage, fraction_voltage);
+				}
+				
+				usb_cdc_write((uint8_t *)buffer, len);	
+				reading_nV_sum = 0;
+				sample_count = 0;
+				missed_reading = false;
 			}
+
 			analog_ready_data = false;
 		}
 	}
